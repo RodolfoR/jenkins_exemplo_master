a455e37d264a91b21682c861244014f45f8c48fe
RodolfoR <rodolfo_una@hotmail.com>
2016-03-23 11:18:05 -0300

    Comitando o codigo

---
+++ src/test/java/ImpostoTest.java
@@ -0,0 +1,24 @@
+//import br.com.caelum.blog.jenkins.Imposto;
+//import br.com.caelum.blog.jenkins.Orcamento;
+import static org.junit.Assert.*;
+//package br.com.caelum.blog.jenkins;
+//import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author RODOLFO ROCHA
+ */
+import org.junit.Test;
+
+import br.com.caelum.blog.jenkins.Imposto;
+import br.com.caelum.blog.jenkins.Orcamento;
+
+public class ImpostoTest {
+
+    @Test
+    public void deveCalcularImpostoDeDezPorCento() {
+        Orcamento orcamento = new Orcamento(100.0);
+        Imposto imposto = new Imposto();
+        assertEquals(10.0, imposto.calcula(orcamento), Double.MIN_VALUE);
+    }
+}
